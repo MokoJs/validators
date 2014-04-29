8c44e183cd3b022ebe2ecbd5583591bc58cfec1c
Ryan Schmukler <rschmukler@gmail.com>
2014-04-29 16:41:38 -0400

    add type and instance validators

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var required = require('./lib/required.js'),
-    confirms = require('./lib/confirms.js');
+    confirms = require('./lib/confirms.js'),
+    type     = require('./lib/type.js'),
+    instance = require('./lib/instance.js');
 
 module.exports = function(Model) {
   var check = checkAttr.bind(Model);
@@ -12,5 +14,8 @@ function checkAttr(attr) {
 
   if(opts.required) this.use(required(attr));
   if(opts.confirms) this.use(confirms(attr, opts.confirms));
+
+  if(typeof opts.type == 'string')   this.use(type(attr, opts.type));
+  if(typeof opts.type == 'function') this.use(instance(attr, opts.type));
 }
 
--- test/validators.js
+++ test/validators.js
@@ -1,23 +1,27 @@
 require('co-mocha');
 
 var expect = require('expect.js'),
-    validators = require('../'),
-    moko = require('moko');
+    validators = require('../');
 
 describe('Moko validators', function() {
   var User;
+  before(function() {
+    moko = require('moko');
+    moko.use(validators);
+  });
 
   describe('basic validations', function() {
     describe('required', function() {
       before(function() {
         User = moko('User').attr('name', { required: true });
-        User.use(validators);
       });
 
       it('adds an error if field is missing', function*() {
         var user = yield new User();
         expect(yield user.isValid()).to.be(false);
         expect(user.errors('name')).to.eql(['field required']);
+        user.name = 'Bob';
+        expect(yield user.isValid()).to.be(true);
       });
     });
 
@@ -26,7 +30,6 @@ describe('Moko validators', function() {
         User = moko('User')
           .attr('password', { required: true })
           .attr('passwordConfirmation', { confirms: 'password' });
-        User.use(validators);
       });
 
       it('adds an error if the fields dont match', function*() {
@@ -34,6 +37,42 @@ describe('Moko validators', function() {
         user.password = '12345';
         expect(yield user.isValid()).to.be(false);
         expect(user.errors('passwordConfirmation')).to.eql(['does not match password']);
+        user.passwordConfirmation = '12345';
+        expect(yield user.isValid()).to.be(true);
+      });
+    });
+
+    describe('type validators', function() {
+      describe('string-type validator', function() {
+        before(function() {
+          User = moko('User')
+            .attr('name', { type: 'string' });
+        });
+
+        it('adds an error if the field is the wrong type', function*() {
+          var user = yield new User();
+          user.name = 1234;
+          expect(yield user.isValid()).to.be(false);
+          expect(user.errors('name')).to.eql(['must be type string']);
+          user.name = 'Bob';
+          expect(yield user.isValid()).to.be(true);
+        });
+      });
+
+      describe('function-type validator', function() {
+        before(function() {
+          User = moko('User')
+            .attr('name', { type: String });
+        });
+
+        it('adds an error if the field is the wrong type', function*() {
+          var user = yield new User();
+          user.name = 1234;
+          expect(yield user.isValid()).to.be(false);
+          expect(user.errors('name')).to.eql(['must be type string']);
+          user.name = 'Bob';
+          expect(yield user.isValid()).to.be(true);
+        });
       });
     });
   });
